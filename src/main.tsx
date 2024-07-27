70ed6d4f2eef8a22486a268dafcd23d85c05ff67
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-07-27 21:59:53 +0100

    make the authProvider exported from index.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { AuthProvider } from "./common/contexts/authProvider/AuthProvider.tsx";
+import { AuthProvider } from "./common/contexts/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
